8ea646e1c6ab79531494307311c8112bb8e97d34
makentoshhhh <nekitmak@inbox.ru>
2024-02-02 09:44:01 +0300

    2 задача

--- src/Chisla.kt
+++ src/Chisla.kt
@@ -1,3 +1,4 @@
+/*
 fun main() {
     var maxNumber = Int.MIN_VALUE
     var input: String
@@ -9,5 +10,20 @@ fun main() {
             maxNumber = number
     } while (input != "стоп")
 
+    println("$maxNumber")
+}*/
+
+fun main() {
+    val numbers = mutableListOf<Int>()
+    var input: String
+    do {
+        input = readLine() ?: ""
+        val number = input.toIntOrNull()
+        if (number != null) {
+            numbers.add(number)
+        }
+    } while (input != "стоп")
+
+    val maxNumber = numbers.maxOrNull()
     println("$maxNumber")
 }
